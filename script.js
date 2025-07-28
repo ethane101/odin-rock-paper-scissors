61a4e79f77e63428ea6b51d28c09ac8ebcf8b0d8
Ethan David Escario <escarioethandavidh@gmail.com>
2025-07-27 21:35:20 -0400

    Finished the playRound() function and tested that the choice comparison mechanic was functioning correctly. The global variables for scores are also updated correctly in the function.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// global score variables
+let humanScore = 0;
+let computerScore = 0;
+
+// randomly returns rock, paper, or scissors
 function getComputerChoice() {
     let x = Math.floor(Math.random() * 3); // integer from [0, 2]
     switch (x) {
@@ -12,6 +17,41 @@ function getComputerChoice() {
     }
 }
 
+// gets rock, paper, or scissors from the human player (case-insensitive)
 function getHumanChoice() {
-    return prompt("Choose rock, paper, or scissors: ");    
+    return prompt("Choose rock, paper, or scissors: ").toLowerCase();    
+}
+
+// control flow for playing one round of the game
+function playRound(humanChoice, computerChoice) {
+    // see who wins the round
+    let winningPairs = {"rock" : "scissors", "scissors" : "paper", "paper":"rock"};
+    let winner = ""; // set to "human", "computer", or "tie"
+
+    if (winningPairs[humanChoice] == computerChoice) {
+        winner = "human";
+    } else if (winningPairs[computerChoice] == humanChoice) {
+        winner = "computer";
+    } else {
+        winner = "tie";
+    }
+    
+    // award points based on who wins
+    if (winner != "tie") {
+        if (winner == "human") {
+            humanScore++;
+        } else {
+            computerScore++;
+        }
+    }
+    console.log(`Winner: ${winner} <= ${humanChoice} VS. ${computerChoice}`);
+    console.log(`Human: ${humanScore}, Computer: ${computerScore}`);
+}
+
+// test playRound() function
+for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
 }
