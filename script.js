0a40053bd083fc5665c8115e8054117fe3ad50ea
Ethan David Escario <escarioethandavidh@gmail.com>
2025-07-27 21:17:17 -0400

    Finished getComputerChoice() and verified that no unexpected behavior occurs when function is called

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert("script is linked");
+function getComputerChoice() {
+    let x = Math.floor(Math.random() * 3); // integer from [0, 2]
+    switch (x) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+        default:
+            return "unexpected behavior"
+    }
+}
